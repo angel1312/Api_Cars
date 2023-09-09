9318f01213cb4f74f72c1108057a54a8ac1fe07b
Isora Guadalupe Aquino [sistemas desarrollo] <iaquino@qualitas.com.mx>
2023-09-09 05:25:38 -0600

    Pase a producción

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
+#class Config(object):
+#    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Viento1201*@localhost:5432/bdcarsales"
+#    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Viento1201*@localhost:5432/bdcarsales"
+    SQLALCHEMY_DATABASE_URI = "postgresql://bdcarsales_wav3_user:rr8jfEbQVCjmIKKASa7NYHyJXGuwyZHx@dpg-cju4o1p5mpss73booog0-a.oregon-postgres.render.com:5432/bdcarsales_wav3"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
